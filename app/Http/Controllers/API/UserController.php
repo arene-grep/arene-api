43f26e03209df96e4b92023bde78f84de87a63bb
fivaz <s.a.fivaz@gmail.com>
2021-01-09 15:56:46 +0100

    add user controller / save user request

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Exceptions\Message;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SaveUserRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return response(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param SaveUserRequest $request
+     * @return Response
+     */
+    public function store(SaveUserRequest $request)
+    {
+        $user = new User();
+        $user->fill($request->validated())->save();
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            return response($user);
+        } catch (Exception) {
+            return response(null, Response::HTTP_NO_CONTENT);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param SaveUserRequest $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(SaveUserRequest $request, int $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->fill($request->validated())->save();
+            return response(null);
+        } catch (Exception) {
+            return response(Message::FAILED_UPDATE, Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function destroy(int $id)
+    {
+        if (User::destroy($id))
+            return response(null);
+        else
+            return response(Message::FAILED_DELETED, Response::HTTP_NOT_FOUND);
+    }
+}
