650303bb8c3567e446bf07fa63027e715a6aca02
fivaz <s.a.fivaz@gmail.com>
2021-01-09 15:56:37 +0100

    add event controller / save event request

---
+++ app/Http/Controllers/API/EventController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Exceptions\Message;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SaveEventRequest;
+use App\Models\Event;
+use Exception;
+use Illuminate\Http\Response;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return response(Event::all(), Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param SaveEventRequest $request
+     * @return Response
+     */
+    public function store(SaveEventRequest $request)
+    {
+        $event = new Event();
+        $event->fill($request->validated())->save();
+        return \response($event, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id)
+    {
+        try {
+            $category = Event::findOrFail($id);
+            return response($category);
+        } catch (Exception) {
+            return response(null, Response::HTTP_NO_CONTENT);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param SaveEventRequest $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(SaveEventRequest $request, int $id)
+    {
+        try {
+            $category = Event::findOrFail($id);
+            $category->fill($request->validated())->save();
+            return response(null);
+        } catch (Exception) {
+            return response(Message::FAILED_UPDATE, Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function destroy(int $id)
+    {
+        //TODO remove from event_user too
+        if (Event::destroy($id))
+            return response(null);
+        else
+            return response(Message::FAILED_DELETED, Response::HTTP_NOT_FOUND);
+    }
+}
