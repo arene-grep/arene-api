e23d8d54f00d2cd4b59639239ef6e84384def995
Matthieu Miglioranza <Aldenaine@github.com>
2021-02-28 17:07:08 +0100

    modification Buy.php en buy->Buy

--- app/Models/Buy.php
+++ app/Models/Buy.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class buy extends Model
+class Buy extends Model
 {
     use HasFactory;
     use SoftDeletes;
