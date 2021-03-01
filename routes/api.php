372bd0d03bfd22c6456b95aeab9f683f2c110b4e
fivaz <31249205+fivaz@users.noreply.github.com>
2021-03-01 01:03:25 +0100

    fix: Get routes shouldn't require authentication

--- routes/api.php
+++ routes/api.php
@@ -20,31 +20,58 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::apiResources([
-        'products' => ProductController::class,
-        'categories' => CategoryController::class,
-        'tcgames' => TradingCardGameController::class,
-        'languages' => LanguageController::class,
-        'events' => EventController::class,
-        'users' => UserController::class,
-        'orders' => OrderController::class,
-        'buys' => BuyController::class,
-    ]);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
-    Route::post('/logout', [UserController::class, 'logout']);
-});
+Route::get('/tcgames', [TradingCardGameController::class, 'index']);
+Route::get('/tcgames/{id}', [TradingCardGameController::class, 'show']);
 
-Route::get('/products', [ProductController::class,'index']);
-Route::get('/categories', [CategoryController::class,'index']);
-Route::get('/tcgames', [TradingCardGameController::class,'index']);
-Route::get('/languages', [LanguageController::class,'index']);
-Route::get('/events', [EventController::class,'index']);
+Route::get('/languages', [LanguageController::class, 'index']);
+Route::get('/languages/{id}', [LanguageController::class, 'show']);
+
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{id}', [EventController::class, 'show']);
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
 
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+    Route::post('/tcgames', [TradingCardGameController::class, 'store']);
+    Route::put('/tcgames/{id}', [TradingCardGameController::class, 'update']);
+    Route::delete('/tcgames/{id}', [TradingCardGameController::class, 'destroy']);
+
+    Route::post('/languages', [LanguageController::class, 'store']);
+    Route::put('/languages/{id}', [LanguageController::class, 'update']);
+    Route::delete('/languages/{id}', [LanguageController::class, 'destroy']);
+
+    Route::post('/events', [EventController::class, 'store']);
+    Route::put('/events/{id}', [EventController::class, 'update']);
+    Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::post('/logout', [UserController::class, 'logout']);
+
+    Route::post('/orders', [OrderController::class, 'store']);
+    Route::put('/orders/{id}', [OrderController::class, 'update']);
+    Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
+
+    Route::post('/buys', [BuyController::class, 'store']);
+    Route::put('/buys/{id}', [BuyController::class, 'update']);
+    Route::delete('/buys/{id}', [BuyController::class, 'destroy']);
+});
 
 
 
